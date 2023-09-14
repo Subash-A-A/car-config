feb7c45fd4c783f003ddb3a09f46d32564bc7a17
PradhipJ <pradhipkumarj12@gmail.com>
2023-09-14 18:39:05 +0530

    login complete

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Login.scss";
 import login_bg from "../assets/login_bg.png";
+import axios from "axios";
 
 function Login() {
+  const [isUser, setUser] = useState(false);
+  function toggleState() {
+    setUser(!isUser);
+  }
+  async function signIn() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const res = await axios.post("http://localhost:4000/app/login", {
+      username: user,
+      password: pass,
+    });
+
+    if (res.data == "existing user") {
+      console.log("fine");
+    } else {
+      alert(res);
+    }
+  }
+  async function signUp() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    console.log(user, pass, email);
+    const res = await axios.post("http://localhost:4000/app/signup", {
+      username: user,
+      password: pass,
+      email: email,
+    });
+    console.log(res);
+    if (res == "existing user") {
+    } else {
+      alert(res);
+    }
+  }
   return (
     <>
       <div className="bg">
@@ -13,26 +48,62 @@ function Login() {
         <div className="form_container">
           <div className="login_form">
             <div className="login_title">
-              <h1>Sign in</h1>
-              <div>Don't have an account? Sign up</div>
+              {isUser ? (
+                <>
+                  <h1>Sign in</h1>
+                  <div className="toggle_btn" onClick={toggleState}>
+                    Don't have an account? Sign up
+                  </div>
+                </>
+              ) : (
+                <>
+                  <h1>Sign up</h1>
+                  <div className="toggle_btn" onClick={toggleState}>
+                    Already have an account? Sign in
+                  </div>
+                </>
+              )}
             </div>
             <div className="login_fields">
+              {isUser ? (
+                <></>
+              ) : (
+                <input
+                  id="email"
+                  className="input_field"
+                  type="email"
+                  required
+                  placeholder="Email"
+                ></input>
+              )}
               <input
-                id="email"
+                id="username"
                 className="input_field"
-                type="email"
+                type="text"
                 required
-                placeholder="Email"
+                placeholder="Username"
               ></input>
               <input
                 id="password"
                 className="input_field"
-                type="text"
+                type="password"
                 required
                 placeholder="Password"
               ></input>
             </div>
-            <div className="login_button">Sign in</div>
+            {isUser ? (
+              <>
+                <div className="login_button" onClick={signIn}>
+                  Sign in
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="login_button" onClick={signUp}>
+                  Sign up
+                </div>
+              </>
+            )}
           </div>
           <img className="login_bg" src={login_bg} alt="Login Background"></img>
         </div>
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,52 +1,52 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const Signuptemplatecopy = require("../models/signupModel")
-const bcrypt=require('bcrypt')
+const Signuptemplatecopy = require("../models/signupModel");
+const bcrypt = require("bcrypt");
 
-router.get('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    Signuptemplatecopy.findOne({ username: username })
-        .then(async existingUser => {
-            if (existingUser!==null) {
-                const result = await bcrypt.compare(password, existingUser.password);
-                if (result) {
-                    res.send("existing user");
-                } else {
-                    res.send("password wrong");
-                }
-            } else {
-                res.send("no username found");
-            }
-        });
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(req);
+  Signuptemplatecopy.findOne({ username: username }).then(
+    async (existingUser) => {
+      if (existingUser !== null) {
+        const result = await bcrypt.compare(password, existingUser.password);
+        if (result) {
+          res.send("existing user");
+        } else {
+          res.send("password wrong");
+        }
+      } else {
+        res.send("no username found");
+      }
+    }
+  );
 });
 
-router.post('/signup', (req, res) => {
-    const { username, email, password } = req.body;
+router.post("/signup", (req, res) => {
+  const { username, email, password } = req.body;
 
-    Signuptemplatecopy.findOne({ username:username,email: email })
-        .then(existingUser => {
-            if (existingUser) {
-                return res.status(400).json({ error: 'Email already exists' });
-            }
-            const signupUser = new Signuptemplatecopy({
-                username: username,
-                email: email,
-                password: password,
-            });
+  Signuptemplatecopy.findOne({ username: username, email: email })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res.status(400).json({ error: "Email already exists" });
+      }
+      const signupUser = new Signuptemplatecopy({
+        username: username,
+        email: email,
+        password: password,
+      });
 
-            return signupUser.save();
-        })
-        .then(data => {
-            res.json(data);
-            console.log(data);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'An error occurred' });
-        });
+      return signupUser.save();
+    })
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred" });
+    });
 });
-
 
 module.exports = router;
