8d66af57d5a19b7e24836d81ca60279700f41951
HariharanCP03 <justbecool0306@gmail.com>
2023-09-14 18:31:54 +0530

    server code changes

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
-const dotenv=require('dotenv')
-const cors= require('cors')
-const routeUrls=require('./routes/router')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const routeUrls = require('./routes/router');
 
+dotenv.config();
 
-dotenv.config()
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.DATABASE_ACCESS, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+}
+connectToDatabase();
 
-mongoose.connect(process.env.DATABASE_ACCESS)
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+app.use('/app', routeUrls);
 
-app.use('/app',routeUrls)
-app.listen(4000,()=>console.log("server is running"));
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+});
